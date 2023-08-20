a4c8404d9aa3b6c386297662c180da0d93a1ac5e
Miguel <miguel.viloria.247@gmail.com>
2023-08-20 00:32:24 -0400

    env port

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const app = require('../app');
 
+const PORT =  process.env.PORT || 3000
+
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(PORT);
 
 server.on('listening', () => {
     console.log('Servidor escuchando en el puerto 3000')
