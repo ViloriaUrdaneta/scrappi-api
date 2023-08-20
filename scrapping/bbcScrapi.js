aa736c35f7ac5483e6b6418efeb6a8833fb7d0de
Miguel <miguel.viloria.247@gmail.com>
2023-08-20 00:19:07 -0400

    args

--- scrapping/bbcScrapi.js
+++ scrapping/bbcScrapi.js
@@ -1,10 +1,16 @@
 const puppeteer = require("puppeteer");
-
+require("dotenv").config()
 
 const bbcNews = async () => {
 
     try {
         const browser = await puppeteer.launch({
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
             executablePath: 
                 process.env.NODE_ENV === 'production' 
                     ? process.env.PUPPETEER_EXECUTABLE_PATH
